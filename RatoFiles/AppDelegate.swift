c70c733e64fa07f9c5be83bf9ab7cf5c4c6afafe
sayzim28-pixel <sayzim28@gmail.com>
2026-09-07 20:04:56 -0300

    Simplify root view controller setup

--- RatoFiles/AppDelegate.swift
+++ RatoFiles/AppDelegate.swift
@@ -6,9 +6,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         window = UIWindow(frame: UIScreen.main.bounds)
-        let mainVC = MainViewController()
-        let navController = UINavigationController(rootViewController: mainVC)
-        window?.rootViewController = navController
+        window?.rootViewController = MainViewController()
         window?.makeKeyAndVisible()
         return true
     }
